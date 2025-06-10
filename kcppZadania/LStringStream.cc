adc8ac76804c33979472a5f65b2c9ab51fd4e42b
Gabisia <paluszek147@gmail.com>
2025-06-10 23:42:16 +0200

    Zadania

---
+++ kcppZadania/LStringStream.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+    string tekst = "123 45.67 C++";
+    stringstream ss(tekst);
+
+    int liczba;
+    float zmiennoprzecinkowa;
+    string jezyk;
+
+    ss >> liczba >> zmiennoprzecinkowa >> jezyk;
+
+    cout << "Odczytano ze stringstream:" << endl;
+    cout << "Liczba calkowita: " << liczba << endl;
+    cout << "Liczba zmiennoprzecinkowa: " << zmiennoprzecinkowa << endl;
+    cout << "Slowo: " << jezyk << endl;
+
+    int a = 2025;
+    stringstream konwerter;
+    konwerter << "Rok: " << a;
+
+    string wynik = konwerter.str();
+    cout << "\nZawartosc konwertera: " << wynik << endl;
+
+    string dane = "999";
+    int liczbaZStringa;
+    stringstream(dane) >> liczbaZStringa;
+    cout << "\nZamieniony string na int: " << liczbaZStringa << endl;
+
+    return 0;
+}
